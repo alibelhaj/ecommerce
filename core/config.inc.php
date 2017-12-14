955ffa81348b7898067945fc8a13bfba8cc628fe
ALI <ali.belhadj@niji.fr>
2017-12-14 23:43:07 +0100

    Add ORM ReadBean

---
+++ core/config.inc.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Database
+ */
+
+define('DB_TYPE', 'mysql');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'ecommerce');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+
+/**
+ * Readbean configuration
+ */
+
+define('READBEAN_MODEL_PREFIX', '\\Ecommerce\\Model\\');
